b562c52fc2d550f47fe5678416a603f622e84090
Jesse Glover <jglover88@hotmail.com>
2023-09-05 14:32:14 -0500

    Removed Code moved to new locations.

--- AuthResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SalesforceConnectedWithCSharp
-{
-    public class AuthResponse
-    {
-        public string access_token { get; set; }
-        public string instance_url { get; set; }
-    }
-}
