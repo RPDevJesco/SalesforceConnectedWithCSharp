e398402c8a8553305a4562d00bff91968f330b9d
Jesco <jglover88@hotmail.com>
2023-09-05 11:48:04 -0500

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -2,6 +2,8 @@
 {
     class Constants
     {
+        // We simplify the salesforce version to be it's own string to make it so updates are easier.
+        public const string SalesforceVersionNumber = "58.0";
         public const string CONSUMER_KEY = "ConnectedAppConsumerKey";
         public const string CONSUMER_SECRET = "ConnectedAppConsumerSecret";
         public const string USERNAME = "SalesforceLoginUserName";
@@ -11,7 +13,9 @@
         // use https://test.salesforce.com/services/oauth2/token if sandbox / scratch org
         // the below endpoint is the developer free edition endpoint.
         public const string TOKEN_REQUEST_ENDPOINTURL = "https://na18-dev-ed.develop.my.salesforce.com/services/oauth2/token";
-        public static string TOKEN_REQUEST_QUERYURL = "/services/data/v58.0/query?q=";
-        public static string TOKEN_REQUEST_POSTURL = "/services/data/v58.0/sobjects/";
+        public static string TOKEN_REQUEST_QUERYURL = "/services/data/v" + SalesforceVersionNumber + "/query?q=";
+        public static string TOKEN_REQUEST_POSTURL = "/services/data/v" + SalesforceVersionNumber + "/sobjects/";
+        public static string JOB_ENDPOINT = $"/services/async/" + SalesforceVersionNumber + "/job";
+        public static string JOB_STATUS = $"/services/data/v" + SalesforceVersionNumber + "/jobs/ingest/";
     }
 }
